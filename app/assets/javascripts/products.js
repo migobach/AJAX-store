61a4d77ccdfba2ec3e90630f68eeed4202370095
Michael Bach <migobach@gmail.com>
2018-06-08 17:29:31 -0600

    add GET all items

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,7 +1,25 @@
-var currentSession = {};
+var currentProduct = {};
 
-function 
+function listProducts() {
+
+}
 
 $(document).ready( function() {
-
+  $.ajax({
+    url: 'http://json-server.devpointlabs.com/api/v1/products',
+    method: 'GET',
+    dataType: 'JSON'
+  }).done( function(products) {
+    debugger
+      products.forEach( function(prod) {
+        // if (prod.base_price == null ? 0 : false) 
+        var li = '<li data-prod-id="'+ prod.id +'" class="collection-item">' + prod.name +  '<br>' + '$' + prod.base_price + '<br>'  + prod.description + '<br>' + 'Quantity: ' + prod.quantity_on_hand + '</li>'
+        $('#product-list').append(li)
+      })
+  })
 })
+ 
+
+// characters.forEach( function(char) {
+//   var li = '<li data-character-id="' + char.id + '">' + char.name + '-' + char.power + '</li>'
+//   list.append(li)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'stores/index'
-  get 'stores/show'
-  get 'stores/create'
-  get 'stores/update'
-  get 'stores/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'products#index'
+
+  resources :products 
 end
