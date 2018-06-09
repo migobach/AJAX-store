fa66a2a42b1b818e3426c43880d7c981079aa6d6
Michael Bach <migobach@gmail.com>
2018-06-08 19:08:15 -0600

    add post

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -5,21 +5,43 @@ function listProducts() {
 }
 
 $(document).ready( function() {
+
   $.ajax({
     url: 'http://json-server.devpointlabs.com/api/v1/products',
     method: 'GET',
     dataType: 'JSON'
   }).done( function(products) {
-    debugger
       products.forEach( function(prod) {
         // if (prod.base_price == null ? 0 : false) 
         var li = '<li data-prod-id="'+ prod.id +'" class="collection-item">' + prod.name +  '<br>' + '$' + prod.base_price + '<br>'  + prod.description + '<br>' + 'Quantity: ' + prod.quantity_on_hand + '</li>'
         $('#product-list').append(li)
       })
   })
-})
- 
 
-// characters.forEach( function(char) {
-//   var li = '<li data-character-id="' + char.id + '">' + char.name + '-' + char.power + '</li>'
-//   list.append(li)
+
+  $('#submit_button').on('click', function() {
+    var name = $('#name').val()
+    var base_price = $('#price').val()
+    var description = $('#desc').val()
+    var prod = { product: {name: name, base_price: base_price, description: description } }
+   
+    // AJAX REQUEST POSTING TO SERVER 
+    $.ajax({
+      url: 'http://json-server.devpointlabs.com/api/v1/products',
+      type: 'POST',
+      datatype: 'JSON',
+      data: prod 
+    })
+  
+    // FAKING THE USER TO BELIEVE THE PAGE UPDATED
+    .done( function(prod) {
+      var li = '<li data-prod-id="'+ prod.id +'" class="collection-item">' + prod.name +  '<br>' + '$' + prod.base_price + '<br>'  + prod.description + '<br>' + 'Quantity: ' + prod.quantity_on_hand + '</li>'
+      $('#product-list').append(li)
+     
+    // CLEARING INPUT VALUES 
+      $('#name').val('')
+      $('#base_price').val('')
+      $('#description').val('')
+    })
+  })
+})
