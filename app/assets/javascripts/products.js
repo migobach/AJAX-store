aa7fe0f60e264a8a66aeb8a7a0bfce3b95ae9db1
Michael Bach <migobach@gmail.com>
2018-06-09 16:24:23 -0600

    add edit form fill

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -16,6 +16,17 @@ $(document).ready( function() {
     })
   })
 
+  // original up above
+
+
+
+
+  //1: get item information from the server @ id
+      //2: populate the form from the server item 
+      //3: @ submit adjust POST method with a PUT statement use a ternary
+      //4: refresh the data in the list
+
+  // original down below
 
   $('#submit_button').on('click', function() {
     var name = $('#name').val()
@@ -43,92 +54,75 @@ $(document).ready( function() {
     })
   })
 
-  // here is how I am supposed to be able to edit EDIT FUNCTION
+  //  here is how I am supposed to be able to edit EDIT FUNCTION
 
-  $(document).on('click', '.edit', function(prod) {
-    editId = this.parentNode.dataset.prodId
-    debugger
-    $('#name') = prod.name
-    $('#price') = prod.base_price
-    $('#description') = prod.description
-    debugger
-    // setting the valus in the form to the selected product values
-      var name = $('#name').val()
-      var base_price = $('#price').val()
-      var description = $('#desc').val()
-      var id = prod.id
-      debugger
-      var prodNew = { product: { id: id, name: name, base_price: base_price, description: description } }
-    $.ajax({
-      url: 'http://json-server.devpointlabs.com/api/v1/products/' + prod.id,
-      type: 'POST',
-      dataType: 'JSON',
-      data: prodNew
-//make a cancel button and have that be the else
+    $(document).on('click', '.edit', function(prod) {
+      editId = this.parentNode.dataset.prodId
+      $.ajax({
+        url: 'http://json-server.devpointlabs.com/api/v1/products/' + editId,
+        method: 'GET',
+        dataType: 'JSON'
+      }).done( function(product){
+        $('#name').val(`${product.name}`)
+        $('#price').val(`${product.base_price}`)
+        $('#desc').val(`${product.description}`)
+        editId = product.id
+        debugger
+      })
+    })
 
-    }).done(function (prodNew) {
-      var li = '<li data-prod-id="'+ prod.id +'" class="collection-item">' + prod.name +  '<br>' + '$' + prod.base_price + '<br>'  + prod.description + '<br>' + 'Quantity: ' + prod.quantity_on_hand + '<br>' + '</li>'
-      $('#product-list').append(li)
-      $('#name').val('')
-      $('#base_price').val('')
-      $('#description').val('')
-    }).fail( function(err) {
-      alert(err.responseJSON.errors)
-    });
-  })
+    //   debugger
+    //   $('#name') = prod.name
+    // $('#price') = prod.base_price
+    // $('#description') = prod.description
+  
 
-  $(document).on('click', '.delete', function(prod) {
-    // debugger
+
+
+    // // setting the valus in the form to the selected product values
+    //   var name = $('#name').val()
+    //   var base_price = $('#price').val()
+    //   var description = $('#desc').val()
+    //   var id = prod.id
+    //   debugger
+    //   var prodNew = { product: { id: id, name: name, base_price: base_price, description: description } }
+    // $.ajax({
+    //   url: 'http://json-server.devpointlabs.com/api/v1/products/' + prod.id,
+    //   type: 'POST',
+    //   dataType: 'JSON',
+    //   data: prodNew
+
+
+      //make a cancel button and have that be the else
+    // }).done(function (prodNew) {
+    //   var li = '<li data-prod-id="'+ prod.id +'" class="collection-item">' + prod.name +  '<br>' + '$' + prod.base_price + '<br>'  + prod.description + '<br>' + 'Quantity: ' + prod.quantity_on_hand + '<br>' + '</li>'
+    //   $('#product-list').append(li)
+    //   $('#name').val('')
+    //   $('#base_price').val('')
+    //   $('#description').val('')
+    // }).fail( function(err) {
+    //   alert(err.responseJSON.errors)
+    // })
+
+  // DELETE function
+
+  $(document).on('click', '.delete', function() {
     delId = this.parentNode.dataset.prodId
     $.ajax({
       url: 'http://json-server.devpointlabs.com/api/v1/products/' + delId,
       method: 'DELETE'
-    }).done( function() {
+    }).done( function(msg) {
       var row = $("[data-prod-id='" + delId + "'")
+      // debugger
       row.remove('li');
-    });
-  } )
+      alert(msg.message)
+
+
+      //1: get item information from the server @ id
+      //2: populate the form from the server item 
+      //3: @ submit adjust POST method with a PUT statement use a ternary
+      //4: refresh the data in the list
+    })
+  })
 })
-
-      // debugger 
-
-
-//   editingGame = $(this).siblings('.game-item').data().id
-//   toggleForm();
-// });
-// $(document).on('submit', '#game-form form', function(e) {
-//   e.preventDefault(); //stops from going to server
-//   var data = $(this).serializeArray();
-//   var url = '/games';
-//   var method = 'POST'
-//   if (editingGame) {
-//     url = url + '/' + editingGame;
-//     method = 'PUT'
-//     debugger
-  // _ajax_request(url, data, callback, method) {
-  //   return jQuery.ajax({
-  //       url: url,
-  //       type: method,
-  //       data: data,
-  //       success: callback
-  //   })
-
-
-// $(document).on('click', '.game-item', function() {
-//   currentGame.id = this.dataset.id
-//   currentGame.name = this.dataset.name
-//   $.ajax({
-//     url: '/games/' + currentGame.id + '/characters',
-//     method: 'GET',
-//     dataType: 'JSON'
-//   }).done( function(characters) {
-//     $('#game').text('Characters in ' + currentGame.name);
-//     var list = $('#characters');
-//     list.empty();
-//     characters.forEach( function(char) {
-//       var li = '<li data-character-id="' + char.id + '">' + char.name + '-' + char.power + '</li>'
-//       list.append(li)
-//     });
-//   });
-// });
 
