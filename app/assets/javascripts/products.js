37923a817ce82a208434347dbc553e9a6a257882
Michael Bach <migobach@gmail.com>
2018-06-09 18:00:11 -0600

    add full edit function

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -18,7 +18,6 @@ $(document).ready( function() {
     })
   })
 
-  // original up above
 
   $(document).on('click', '.edit', function(prod) {
     editId = this.parentNode.dataset.prodId
@@ -31,25 +30,19 @@ $(document).ready( function() {
       $('#price').val(`${product.base_price}`)
       $('#desc').val(`${product.description}`)
       editId = product.id
+      debugger
     })
   })
 
-  //1: get item information from the server @ id
-      //2: populate the form from the server item 
-      //3: @ submit adjust POST method with a PUT statement use a ternary
-      //4: refresh the data in the list
-
-  // original down below
 
   $('#submit_button').on('click', function() {
     editId
     var name = $('#name').val()
     var base_price = $('#price').val()
     var description = $('#desc').val()
-    var urlVal = ( editId == null ) ? 'http://json-server.devpointlabs.com/api/v1/products' : 'http://json-server.devpointlabs.com/api/v1/products' + editId
+    var urlVal = ( editId == null ) ? 'http://json-server.devpointlabs.com/api/v1/products' : 'http://json-server.devpointlabs.com/api/v1/products/' + editId
     var typeVal = ( editId == null ) ? 'POST' : 'PUT'
-    var prodVal = (editId == null ) ? { product: {name: name, base_price: base_price, description: description } } : { product: {id: prodId, name: name, base_price: base_price, description: description } } 
-    debugger
+    var prodVal = (editId == null ) ? { product: {name: name, base_price: base_price, description: description } } : { product: { base_price: base_price, color: null, description: description, id: editId, name: name, other_attributes: null, quantity_on_hand: 0, weight: null } } 
 
     $.ajax({
           url: urlVal, 
@@ -57,25 +50,7 @@ $(document).ready( function() {
           datatype: 'JSON',
           data: prodVal 
         })
-
-
-    // AJAX REQUEST POSTING TO SERVER 
-    // if (editId = null ) {
-    //   debugger
-    //   $.ajax({
-    //     url: 'http://json-server.devpointlabs.com/api/v1/products',
-    //     type: 'POST',
-    //     datatype: 'JSON',
-    //     data: prod 
-    //   })
-    // }
-    // else
-    //   $.ajax({
-    //     url: 'http://json-server.devpointlabs.com/api/v1/products' + prodId, 
-    //     type: 'PUT',
-    //     datatype: 'JSON',
-    //     data: prod
-    //   })
+      
   
     // FAKING THE USER TO BELIEVE THE PAGE UPDATED
     .done( function(prod) {
@@ -89,44 +64,7 @@ $(document).ready( function() {
     })
   })
 
-  //  here is how I am supposed to be able to edit EDIT FUNCTION
-
     
-
-    //   debugger
-    //   $('#name') = prod.name
-    // $('#price') = prod.base_price
-    // $('#description') = prod.description
-  
-
-
-
-    // // setting the valus in the form to the selected product values
-    //   var name = $('#name').val()
-    //   var base_price = $('#price').val()
-    //   var description = $('#desc').val()
-    //   var id = prod.id
-    //   debugger
-    //   var prodNew = { product: { id: id, name: name, base_price: base_price, description: description } }
-    // $.ajax({
-    //   url: 'http://json-server.devpointlabs.com/api/v1/products/' + prod.id,
-    //   type: 'POST',
-    //   dataType: 'JSON',
-    //   data: prodNew
-
-
-      //make a cancel button and have that be the else
-    // }).done(function (prodNew) {
-    //   var li = '<li data-prod-id="'+ prod.id +'" class="collection-item">' + prod.name +  '<br>' + '$' + prod.base_price + '<br>'  + prod.description + '<br>' + 'Quantity: ' + prod.quantity_on_hand + '<br>' + '</li>'
-    //   $('#product-list').append(li)
-    //   $('#name').val('')
-    //   $('#base_price').val('')
-    //   $('#description').val('')
-    // }).fail( function(err) {
-    //   alert(err.responseJSON.errors)
-    // })
-
-  // DELETE function
 
   $(document).on('click', '.delete', function() {
     delId = this.parentNode.dataset.prodId
@@ -139,11 +77,6 @@ $(document).ready( function() {
       row.remove('li');
       alert(msg.message)
 
-
-      //1: get item information from the server @ id
-      //2: populate the form from the server item 
-      //3: @ submit adjust POST method with a PUT statement use a ternary
-      //4: refresh the data in the list
     })
   })
 })
